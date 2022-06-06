e9b64262e28bbea7506593360815e7366619aaf6
Nu <nu.chai56@gmail.com>
2022-06-06 00:15:31 -0500

    style media link

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+const github = document.getElementsByClassName("fa-github")
+const linkedin = document.getElementsByClassName("fa-linkedin")
+const facebook = document.getElementsByClassName("fa-facebook")
+const twitter = document.getElementsByClassName("fa-twitter")
+const instagram = document.getElementsByClassName("fa-instagram")
+
