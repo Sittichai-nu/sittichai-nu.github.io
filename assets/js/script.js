f81719b62794a562101694f11bde081e7a65dc17
Nu <nu.chai56@gmail.com>
2022-06-07 12:38:12 -0500

    edia styling

--- assets/js/script.js
+++
@@ -1,6 +0,0 @@
-const github = document.getElementsByClassName("fa-github")
-const linkedin = document.getElementsByClassName("fa-linkedin")
-const facebook = document.getElementsByClassName("fa-facebook")
-const twitter = document.getElementsByClassName("fa-twitter")
-const instagram = document.getElementsByClassName("fa-instagram")
-
