291e8ff95b5e5c8ff74d42a42cf346a3319f0fe4
Nu <nu.chai56@gmail.com>
2022-07-30 13:51:08 -0500

    updated active tab

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,22 @@
 const allBtnLink = document.querySelectorAll(".control");
 const contList = document.querySelector(".my-lists");
-const allSect = document.querySelector(".section");
+const allSect = document.querySelectorAll(".section");
 
 // Link button
 contList.addEventListener("click", function (e) {
+  e.preventDefault();
   const clicked = e.target.closest(".control");
   console.log(clicked);
   if (!clicked) return;
-  allBtnLink.forEach(i => i.classList.remove("active-btn"));
+
+  //Remove activate tab
+  allBtnLink.forEach((i) => i.classList.remove("active-btn"));
+  allSect.forEach((i) => i.classList.remove("section-active"));
+
+
+  // Activate tab
   clicked.classList.add("active-btn");
+  document
+    .querySelector(`.operetor__section--${clicked.dataset.tab}`)
+    .classList.add("section-active");
 });
