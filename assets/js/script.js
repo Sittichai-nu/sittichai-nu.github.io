66cd894898775cae2da3da068f893e664bcdbd89
Nu <nu.chai56@gmail.com>
2022-07-30 19:25:24 -0500

    created secton details

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,13 +6,11 @@ const allSect = document.querySelectorAll(".section");
 contList.addEventListener("click", function (e) {
   e.preventDefault();
   const clicked = e.target.closest(".control");
-  console.log(clicked);
   if (!clicked) return;
 
   //Remove activate tab
   allBtnLink.forEach((i) => i.classList.remove("active-btn"));
   allSect.forEach((i) => i.classList.remove("section-active"));
-
 
   // Activate tab
   clicked.classList.add("active-btn");
